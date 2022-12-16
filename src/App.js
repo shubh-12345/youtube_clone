e84b20d9ef940d0e45c2da753b73c845f40977cd
shubh-12345 <104341752+shubh-12345@users.noreply.github.com>
2022-12-16 21:03:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
